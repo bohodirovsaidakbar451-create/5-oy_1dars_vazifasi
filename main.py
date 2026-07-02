71d90d6c7e85d3108dccfca7af4db3bc9cdf2a2e
SaidAkbar <bahodirovsaidakbar451@gmail.com>
2026-07-02 15:13:04 +0500

    Arifmetik funksiyalar qo'shildi

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ def kvadrat(a):
 
 def kub(a):
     return a * a * a
+
+def yigindi(a, b):
+    return a + b
+
+
+def ayirma(a, b):
+    return a - b
+
+
+def kopaytma(a, b):
+    return a * b
+
+
+def bolish(a, b):
+    return a / b
